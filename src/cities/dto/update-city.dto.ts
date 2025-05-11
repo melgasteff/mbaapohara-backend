c8880d4b48729e886a2e25e88242a2f2be144176
melgasteff <melgasteff@gmail.com>
2025-05-11 15:11:37 -0300

    chore: eliminacion de archivos obsoletos de City

--- src/cities/dto/update-city.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCityDto } from './create-city.dto';
-import { Country } from 'src/countries/entities/country.entity';
-
-export class UpdateCityDto extends PartialType(CreateCityDto) {
-    descripcion?: string
-    pais?: Country
-}
