074e99fbcbe8062740e72df9680e335250c42b20
melgasteff <melgasteff@gmail.com>
2025-06-04 13:40:17 -0300

    feat: enable Cors

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { GlobalExceptionFilter } from './cities/infraesructure/exception-filter/exceptionFilter';
+import { GlobalExceptionFilter } from './cities/infrastructure/exception-filter/exceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalFilters(new GlobalExceptionFilter());
 
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
     whitelist: true,
     forbidNonWhitelisted: true
   }));
-
+  app.enableCors()
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
