67f3ea094ad63e15a44e265f38e32a0943bdbed7
melgasteff <melgasteff@gmail.com>
2025-09-07 14:30:42 -0300

    feat: agregar validacion is number

--- src/evaluations/presentation/dto/new-evaluation-detail.dto.ts
+++ src/evaluations/presentation/dto/new-evaluation-detail.dto.ts
@@ -1,14 +1,17 @@
-import { IsNotEmpty } from "class-validator"
+import { IsNotEmpty, IsNumber } from "class-validator"
 
 export class NewEvaluationDetailDTO {
 
     @IsNotEmpty()
+    @IsNumber()
     idevaluation: number
 
     @IsNotEmpty()
+    @IsNumber()
     iditem: number
 
     @IsNotEmpty()
+    @IsNumber()
     rating: number
 
     extraReason?: string
