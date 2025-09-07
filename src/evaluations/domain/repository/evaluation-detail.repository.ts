391d5e99b595e5fceb54f4367e31ab13618d0e87
melgasteff <melgasteff@gmail.com>
2025-09-07 13:52:40 -0300

    fix: agregar tipo en getAll

--- src/evaluations/domain/repository/evaluation-detail.repository.ts
+++ src/evaluations/domain/repository/evaluation-detail.repository.ts
@@ -3,7 +3,7 @@ import { NewEvaluationDetail } from "../model/new-evaluation-detail.entity";
 
 export abstract class EvaluationDetailRepository {
     abstract create(newEvaluationDetail: NewEvaluationDetail): Promise<EvaluationDetail>
-    abstract getAll(evaluationid): Promise<EvaluationDetail[]>
+    abstract getAll(evaluationid:number): Promise<EvaluationDetail[]>
     abstract getById(id: number): Promise<EvaluationDetail | null>;
     abstract update(id: number, evaluationDetail: Partial<EvaluationDetail>): Promise<EvaluationDetail>;
     abstract delete(id: number): Promise<void>;
